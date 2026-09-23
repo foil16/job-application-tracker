727e2c02de3e2d3f8fdb1e3ea65cb2fdf769ba95
foil16 <silverm4321@gmail.com>
2026-09-22 22:57:52 -0400

    first Version of the db schema

---
+++ backend/schema.sql
@@ -0,0 +1,25 @@
+CREATE TYPE application_status AS ENUM ('applied','interviewing','offer','rejected');
+
+CREATE TABLE users (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
+    hashed_password TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+
+);
+
+CREATE TABLE applications (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    date_applied DATE DEFAULT CURRENT_DATE NOT NULL,
+    company_name TEXT NOT NULL,
+    role_name TEXT NOT NULL,
+    status application_status NOT NULL DEFAULT 'applied',
+    notes TEXT,
+    user_id INT NOT NULL,
+
+    CONSTRAINT fk_user 
+        FOREIGN KEY(user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+    
+);
